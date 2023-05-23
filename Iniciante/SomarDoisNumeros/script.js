fc3b334d25192c571fb4734df2664ba6d166d800
Sávio <saviodeveloper@proton.me>
2023-05-23 00:22:39 -0300

    Exercício Finalizado
    Update:
    + Botão de limpar
    + Mensagem de erro para operação inválida

--- Iniciante/SomarDoisNumeros/script.js
+++ Iniciante/SomarDoisNumeros/script.js
@@ -1,4 +1,5 @@
 function somarNumeros() {
+    if (EntradaNumero1.value != '' && EntradaNumero2.value != '' ) {
     // Recuperar os valores dos campos de entrada
     var num1 = parseFloat(document.getElementById('EntradaNumero1').value);
     var num2 = parseFloat(document.getElementById('EntradaNumero2').value);
@@ -8,4 +9,18 @@ function somarNumeros() {
 
     // Exibir o resultado
     document.getElementById('Resultado').innerHTML = `<p> Resultado da soma entre ${num1} e ${num2} é: <strong> ${resultado} </strong> </p>`
+
+    } else {
+    document.getElementById('Resultado').innerHTML = `<p> <strong> [Error] </strong> Valor inválido </p>`
+    }
+}
+
+function limparNumeros() {
+    var limparNum1 = document.getElementById('EntradaNumero1')
+    var limparNum2 = document.getElementById('EntradaNumero2')
+
+    limparNum1.value = ''
+    limparNum2.value = ''
+
+    document.getElementById('Resultado').innerHTML = ''
 }
